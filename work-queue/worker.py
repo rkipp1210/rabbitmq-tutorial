fb3e8c560dc30a22285bf98e0b509d1cb174e076
Ross Kippenbrock <rkippenbrock@gmail.com>
2017-02-03 16:16:01 -0500

    work queue example

---
+++ work-queue/worker.py
@@ -0,0 +1,25 @@
+import pika
+import time
+
+connection = pika.BlockingConnection(
+                pika.ConnectionParameters(
+                    'localhost'
+                )
+            )
+
+channel = connection.channel()
+channel.queue_declare(queue='task_queue')
+
+def callback(ch, method, properties, body):
+    print(' [x] Recieved %r' % body)
+    time.sleep(body.count(b'.'))
+    print(' [x]  Done')
+
+channel.basic_consume(
+    callback,
+    queue = 'task_queue',
+    no_ack = True
+)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
